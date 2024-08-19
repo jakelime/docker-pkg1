90d0de2093b91a99d9da46f178863b8eb90f3083
Jake Lim <lim.jiahao@stengg.com>
2024-08-19 15:50:37 +0800

    handle n1 and n2 cases for pip install

--- dockersrc-app/src-django/install.sh
+++ dockersrc-app/src-django/install.sh
@@ -1,3 +1,30 @@
-python3 -m venv venv
-python -m pip install --upgrade pip -i https://artifactreg.stengglink.com/repository/pypi-proxy/simple/
-pip install -r requirements.txt -i https://artifactreg.stengglink.com/repository/pypi-proxy/simple/
+#!/bin/bash
+
+url="https://artifactreg.stengglink.com/"
+url_nexus_pypi="https://artifactreg.stengglink.com/repository/pypi-proxy/simple/"
+
+
+check_nexus_connection() {
+    response=$(curl -s -D - "$url" -o /dev/null)
+    status_code=$(echo "$response" | grep HTTP | awk '{print $2}')    
+    echo "checked $url -> status_code=$status_code"
+}
+
+pip_install_stengg() {
+    python3 -m venv venv
+    python -m pip install --upgrade pip -i $url_nexus_pypi
+    pip install -r requirements.txt -i $url_nexus_pypi
+}
+
+pip_install() {
+    python3 -m venv venv
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+}
+
+
+if [[ $status_code == 200 ]]; then
+    pip_install_stengg
+else
+    pip_install
+fi
